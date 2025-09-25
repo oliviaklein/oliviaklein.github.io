657c8ef7c568750552f3332fc7befa9d645ed4c8
oliviaklein <olkl1421@colorado.edu>
2025-09-25 12:30:20 -0600

    cont

--- WA6/java.js
+++ WA6/java.js
@@ -1,8 +1,6 @@
-// Get all filter buttons and photo cards
 const filterButtons = document.querySelectorAll('.gallery-nav button');
 const photoCards = document.querySelectorAll('.photo-card');
 
-// Add click event to each button
 filterButtons.forEach(button => {
   button.addEventListener('click', (event) => {
     const filterValue = event.target.textContent.toLowerCase();
